88f0a9f6287d811c007bd8fa0de39e91a1c0bc47
Filipe Firmino <filipefirmino@gec.inatel.br>
2025-01-02 10:33:09 -0300

    :fix: ajustes pedidos

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb'
+    }
+  }
 };
 
 export default nextConfig;
