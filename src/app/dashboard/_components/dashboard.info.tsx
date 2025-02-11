802550e51c6e4f0a800746603539c183b7658acb
Filipe Firmino <filipefirmino@gec.inatel.br>
2025-02-11 00:39:29 -0300

    :fix: subindo versão

--- src/app/dashboard/_components/dashboard.info.tsx
+++ src/app/dashboard/_components/dashboard.info.tsx
@@ -279,7 +279,7 @@ export function DashboardInfo() {
                 <BirthdaysV2/>
             </main>
             <footer>
-                Versão: 1.1
+                Versão: 1.2
             </footer>
         </div>
     )
